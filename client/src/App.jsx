3de57d7c28d4c40959eb69116ada07391e731c3e
prabal-007 <gprabal123@gmail.com>
2022-07-06 19:04:17 +0530

    folder structure setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-  const App = () => {
+import { Navbar, Services, Footer, Transactions, Welcome } from "./components"
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar></Navbar>
+        <Welcome></Welcome>
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
