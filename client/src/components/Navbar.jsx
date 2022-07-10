73fc6fdf404954306f0e13142fcb47ae555cfcd6
prabal-007 <gprabal123@gmail.com>
2022-07-10 13:40:17 +0530

    first draft

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,37 +1,33 @@
+import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
-import { useState } from "react";
+
 import logo from "../../images/logo.png";
 
-const NavbarItem = ({ title, classprops}) => {
-    return (
-        <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
-    )
-}
+const NavBarItem = ({ title, classprops }) => (
+  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+);
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
 
-    const [toggleMenu, setToggleMenu] = useState(false);
-    
-    return (
-        <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="md:flex-[0.5] flex-initial justify-center items-center">
-                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
-            </div>
-            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-                {["Market", "Exchange", "Wallets", "Code"].map((item, index) => (
-                    <NavbarItem key={item + index} title={item} />
-                ))}
-                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                    Login
-                </li>
-                {/* <div className="flex relative">
-                    {!toggleMenu
-                    ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false) } />
-                :<HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true) } /> }
-                </div> */}
-            </ul>
-            <div className="flex relative">
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Code", "NFTs"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
+        </li>
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Signup
+        </li>
+      </ul>
+      <div className="flex relative">
         {!toggleMenu && (
           <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
         )}
@@ -44,14 +40,14 @@ const Navbar = () => {
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+            {["Market", "Exchange", "Code", "NFTs"].map(
               (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
             )}
           </ul>
         )}
       </div>
-        </nav>
-    );
-}
+    </nav>
+  );
+};
 
 export default Navbar;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Loader} from "./Loader.jsx";
-export { default as Footer} from "./Footer.jsx";
-export { default as Navbar} from "./Navbar.jsx";
-export { default as Services} from "./Services.jsx";
-export { default as Welcome} from "./Welcome.jsx";
-export { default as Transactions} from "./Transactions.jsx";
+export { default as Loader } from "./Loader";
+export { default as Navbar } from "./Navbar";
+export { default as Welcome } from "./Welcome";
+export { default as Footer } from "./Footer";
+export { default as Services } from "./Services";
+export { default as Transactions } from "./Transactions";
